d587bfe58543c09ccbb939a228056feb72c13ed0
narlae <kyjttaa12@gmail.com>
2022-09-05 19:45:33 +0900

    Queue Stack SLL

---
+++ src/MyStack.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class MyStack<T> {
+    private ArrayList<T> stack = new ArrayList<>();
+
+    public void push(T item) {
+        stack.add(item);
+    }
+
+    public T pop() {
+        if (stack.isEmpty()) {
+            return null;
+        }
+        return stack.remove(stack.size()-1);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        MyStack<Integer> ms = new MyStack<Integer>();
+        ms.push(1);
+        ms.push(2);
+        ms.push(3);
+        System.out.println(ms.pop());
+        System.out.println(ms.pop());
+        System.out.println(ms.pop());
+    }
+}
---
+++ src/SingleLinkedList.java
@@ -0,0 +1,44 @@
+public class SingleLinkedList<T> {
+    public Node<T> head = null;
+
+    public class Node<T> {
+        T data;
+        Node<T> next = null;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public void addNode(T data) {
+        if (head == null) {
+            head = new Node<T>(data);
+        }else {
+            Node<T> node = this.head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<T>(data);
+        }
+    }
+
+    public void printAll() {
+        if (head != null) {
+            Node<T> node = head;
+            System.out.println(node.data);
+            while (node.next != null) {
+                node = node.next;
+                System.out.println(node.data);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SingleLinkedList<Integer> MyLinkedList = new SingleLinkedList<Integer>();
+        MyLinkedList.addNode(1);
+        MyLinkedList.addNode(2);
+        MyLinkedList.addNode(3);
+        MyLinkedList.printAll();
+    }
+}
+
