13635650d6adccfe92449062e4ceb27943b4f733
narlae <kyjttaa12@gmail.com>
2022-09-06 13:15:29 +0900

    DLL

--- src/SingleLinkedList.java
+++ src/SingleLinkedList.java
@@ -33,11 +33,67 @@ public class SingleLinkedList<T> {
         }
     }
 
+    public Node<T> search(T data) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while(node!=null){
+                if(node.data==data){
+                    return node;
+                }else{
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+    public void addNodeInside(T data, T isData) { // data-내가 가진, isData-그 앞의 Node가 가진 data
+        Node<T> searchedNode = this.search(isData); //추가할 노드의 왼쪽에 위치할 Node를 찾는 메서드
+
+        if (searchedNode == null) {
+            this.addNode(data);
+        } else {
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<T>(data);
+            searchedNode.next.next = nextNode;
+        }
+    }
+
+    public boolean delNode(T isData){
+        if(this.head==null){
+            return false;
+        }else{
+            Node<T> node = this.head;
+            if (node.data == isData) {
+                this.head = this.head.next;
+                return true;
+            } else{
+                while(node.next!=null){
+                    if(node.next.data==isData){
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         SingleLinkedList<Integer> MyLinkedList = new SingleLinkedList<Integer>();
         MyLinkedList.addNode(1);
         MyLinkedList.addNode(2);
         MyLinkedList.addNode(3);
+        MyLinkedList.addNode(4);
+        MyLinkedList.addNode(5);
+        MyLinkedList.printAll();
+        System.out.println();
+        MyLinkedList.delNode(3);
+        MyLinkedList.printAll();
+        System.out.println();
+        MyLinkedList.delNode(1);
         MyLinkedList.printAll();
     }
 }
